23407795ba51a4c62867969464cec13662ac1617
BT_1N3_01 <BT_1N3_01@BT_1N3_01.bktech.local>
2017-11-29 13:14:27 -0500

    Close;control the movement of the array items.

--- Lab3.4/src/sorts/ArrayMethods4.java
+++ Lab3.4/src/sorts/ArrayMethods4.java
@@ -4,11 +4,16 @@ public class ArrayMethods4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[]test1= {7,15,4,6,2,8,9,12,14};
+		printIntArray(test1);
+		System.out.println();
+		System.out.println();
+		ArrayMethods4.partition(test1, 4, 7);
+		printIntArray(test1);
 	}
 	public static void partition(int[] list1, int front, int back)
 	{
-		
+		int pivotPos=0;
 		while(front<=back)
 		{
 			int pivot=list1[0];
@@ -20,13 +25,11 @@ public class ArrayMethods4 {
 			{
 				back--;
 			}
-			if(front<=back)
-			{
 				intSwap(list1,front,back);
 				front++;
 				back--;
-			}
 		}
+		intSwap(list1,pivotPos,0);
 	}
 	public static void quickSort(int[] list1, int front, int back)
 	{
@@ -37,5 +40,12 @@ public class ArrayMethods4 {
 		int temp=arr[i];
 		arr[i]=arr[j];
 		arr[j]=temp;
+	}
+	private static void printIntArray(int[]list)
+	{
+		for(int i=0;i<list.length;i++)
+		{
+			System.out.print("["+list[i]+"] ");
+		}
 	}
 }
